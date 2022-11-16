4bf182b83b0ecd842717c0f60b3af9c34805c936
Marcelo Isidorio <marceloisidorio013@gmail.com>
2022-11-16 19:55:54 -0300

    finalizando ex1004

---
+++ Iniciante/1004 - Produto Simples/C/1004 - Produto Simples.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int A, B, PROD;
+    
+    scanf("%d %d", &A, &B); // recebe os valores
+    
+	PROD = A*B; // realiza o cálculo
+    
+    printf("PROD = %d\n", PROD); // apresenta o resultado
+    
+    return 0;
+}
