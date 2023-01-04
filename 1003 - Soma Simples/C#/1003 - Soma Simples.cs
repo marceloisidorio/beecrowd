ccf6a258f4c62d20103e80abbfe7aae6a97f77d4
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-01-04 09:53:08 -0300

    1003 em C#

---
+++ 1003 - Soma Simples/C#/1003 - Soma Simples.cs
@@ -0,0 +1,14 @@
+using System;
+
+class URI {
+    static void Main() {
+        int A, B, SOMA;
+
+        A = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
+
+        SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+    }
+}
