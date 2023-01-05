57d1a49799cb671b07b423df159ef3800b30042b
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-01-05 17:31:22 -0300

    1004 em C#

---
+++ 1004 - Produto Simples/C#/prod.cs
@@ -0,0 +1,14 @@
+using System;
+
+class URI {
+    static void Main() {
+        int A, B, PROD;
+
+        A = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
+
+        PROD = A * B;
+
+        Console.WriteLine("PROD = " + PROD);
+    }
+}
