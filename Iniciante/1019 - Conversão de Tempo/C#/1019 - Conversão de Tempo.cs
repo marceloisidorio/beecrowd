f0e30a59fe682884c0bcc596107c385f9b87359a
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-01-12 12:46:56 -0300

    1019 - Conversão de Tempo em C#

---
+++ Iniciante/1019 - Conversão de Tempo/C#/1019 - Conversão de Tempo.cs
@@ -0,0 +1,14 @@
+using System;
+
+class URI {
+    static void Main() {
+        int tempo = Int32.Parse(Console.ReadLine());
+
+        int horas = tempo/3600;
+        int minutos = (tempo%3600)/60; // resto de horas dividido por minutos
+        int segundos = (tempo%3600)%60; // resto de minutos dividido por segundos
+
+        Console.WriteLine("{0}:{1}:{2}", horas, minutos, segundos);
+
+    }
+}
