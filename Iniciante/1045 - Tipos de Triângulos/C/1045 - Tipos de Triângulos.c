5e7375b093ce5d11babdba421a36d0dd3120c05c
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-02-16 09:04:08 -0300

    1045 em C

---
+++ Iniciante/1045 - Tipos de Triângulos/C/1045 - Tipos de Triângulos.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() { // variáveis
+	double I, J, K;
+	double A, B, C, aux;
+	
+	// entrada
+	scanf("%lf %lf %lf", &A, &B, &C);
+	
+	if(A < B) {
+		aux = A;
+		A = B;
+		B = aux;
+	}
+	if(A < C) { // ordenação dos valores
+		aux = A;
+		A = C;
+		C = aux;
+	}
+	if(B < C) {
+		aux = B;
+		B = C;
+		C = aux;
+	}
+	
+	I = A*A;
+	J = B*B; // atribuição dos quadrados
+	K = C*C;
+	
+	// condicional + saída
+	if(A >= B+C) {
+		printf("NAO FORMA TRIANGULO\n");
+		
+	} else {
+		if(I == J+K) {
+			printf("TRIANGULO RETANGULO\n");
+		}
+		
+		if(I > (J+K)) {
+			printf("TRIANGULO OBTUSANGULO\n");
+		
+		}
+		
+		if(I < J+K) {
+			printf("TRIANGULO ACUTANGULO\n");
+		
+		}
+		
+		if(A == B && B == C) {
+			printf("TRIANGULO EQUILATERO\n");
+		
+		} else if(A == B || B == C || C == A) {
+			printf("TRIANGULO ISOSCELES\n");
+		}
+		
+	}	
+	
+	return 0;
+}
