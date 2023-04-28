936343b60d331a08f8f7de89f1e75ab900abe59d
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-04-28 20:35:54 -0300

    1017 em Python

---
+++ Iniciante/1017 - Gasto de Combustível/Python/1017 - Gasto de Combustível.py
@@ -0,0 +1,9 @@
+# entrada de dados
+tempo = int(input())
+velocidade = int(input())
+
+# processamento / cálculo
+distancia = (velocidade*tempo)/12
+
+# saída de dados
+print('{:.3f}'.format(distancia))
