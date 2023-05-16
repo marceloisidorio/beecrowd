3d6e5250d87bdad9fb857c285a763096642944ee
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-05-15 22:32:43 -0300

    1043 em Python

---
+++ Iniciante/1043 - Triângulo/Python/1043 - Triângulo.py
@@ -0,0 +1,23 @@
+import math
+
+# recebe os valores na mesma linha
+A, B, C = input().split(' ')
+
+# converte de string para float
+A = float(A)
+B = float(B)
+C = float(C)
+
+#       REGRA DO TRIÂNGULO	
+#   | b - c | < a < b + c
+#   | a - c | < b < a + c
+#   | a - b | < c < a + b
+
+if abs(B-C) < A and A < B+C:
+    if abs(A-C) < B and B < A+C:
+        if abs(A-B) < C and C < A+B:
+            perimetro = A+B+C
+            print('Perimetro = {:.1f}'.format(perimetro))
+else:
+    area = ((A+B)*C)/2
+    print('Area = {:.1f}'.format(area))
