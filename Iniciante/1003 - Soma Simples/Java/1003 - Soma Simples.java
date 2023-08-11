d5a1403ecb901e2edb6c4dc8c06bd6d8b596a6f7
Marcelo Isidorio <marceloisidorio013@gmail.com>
2023-08-11 06:20:25 -0300

    1003 em Java

---
+++ Iniciante/1003 - Soma Simples/Java/1003 - Soma Simples.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int a = leitor.nextInt();
+		int b = leitor.nextInt();
+		int soma = a + b;
+		
+		System.out.println("SOMA = " + soma);
+
+	}
+
+}
